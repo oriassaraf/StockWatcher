451af3b6fc7d638f31a0ed98b91d471e3e2ff8e3
Ori <Ori@Ori-HP>
2012-07-11 12:14:36 +0300

    Fix Issue 3 and 4. Ability to Add stock and delete it from the table.
    Also, added the ability to show quote for stocks and refresh them,
    
    Signed-off-by: Ori <Ori@Ori-HP>

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockPrice.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockPrice.java
@@ -1,44 +1,45 @@
 package com.google.gwt.sample.stockwatcher.client;
 
 public class StockPrice {
-	  private String symbol;
-	  private double price;
-	  private double change;
 
-	  public StockPrice() {
-	  }
+  private String symbol;
+  private double price;
+  private double change;
 
-	  public StockPrice(String symbol, double price, double change) {
-	    this.symbol = symbol;
-	    this.price = price;
-	    this.change = change;
-	  }
+  public StockPrice() {
+  }
 
-	  public String getSymbol() {
-	    return this.symbol;
-	  }
+  public StockPrice(String symbol, double price, double change) {
+    this.symbol = symbol;
+    this.price = price;
+    this.change = change;
+  }
 
-	  public double getPrice() {
-	    return this.price;
-	  }
+  public String getSymbol() {
+    return this.symbol;
+  }
 
-	  public double getChange() {
-	    return this.change;
-	  }
+  public double getPrice() {
+    return this.price;
+  }
 
-	  public double getChangePercent() {
-	    return 10.0 * this.change / this.price;
-	  }
+  public double getChange() {
+    return this.change;
+  }
 
-	  public void setSymbol(String symbol) {
-	    this.symbol = symbol;
-	  }
+  public double getChangePercent() {
+    return 10.0 * this.change / this.price;
+  }
 
-	  public void setPrice(double price) {
-	    this.price = price;
-	  }
+  public void setSymbol(String symbol) {
+    this.symbol = symbol;
+  }
 
-	  public void setChange(double change) {
-	    this.change = change;
-	  }
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public void setChange(double change) {
+    this.change = change;
+  }
 }
